8b43ed29dc12657fedbbf4b1c535274b58f3f922
BrandonVazquez259 <brandon.gonzalez@uteca.edu.mx>
2025-07-08 20:34:54 -0600

    Instantiate Tower

---
+++ Assets/Scrips/Tower.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField]
+
+    private Health _health;
+    private Health Health => _health;
+
+    [SerializeField]
+
+    private UnityEvent _onStart;
+
+    public void Initialize()
+    {
+        _onStart?.Invoke();
+    }
+}
